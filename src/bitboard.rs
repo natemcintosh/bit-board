b403681a71df2e3700dca7e1c80787feb467da15
Nate McIntosh <natemcintosh@gmail.com>
2025-10-09 14:52:16 -0400

    first movements towards a common trait

---
+++ src/bitboard.rs
@@ -0,0 +1,115 @@
+use bitvec::slice::BitSlice;
+
+use crate::DimensionMismatch;
+
+pub trait BitBoard {
+    /// Returns the number of rows in the board.
+    fn n_rows(&self) -> usize;
+
+    /// Returns the number of columns in the board.
+    fn n_cols(&self) -> usize;
+
+    /// Returns a mutable reference to the underlying bits.
+    fn board(&mut self) -> &mut BitSlice;
+
+    /// Get the index that we can use to directly access a certain spot on the board
+    fn index_of(&self, row: usize, col: usize) -> usize {
+        assert!(
+            row <= (self.n_rows() - 1),
+            "row cannot be greater than n_rows"
+        );
+        assert!(
+            col <= (self.n_cols() - 1),
+            "col cannot be greater than n_cols"
+        );
+        (row * self.n_cols()) + col
+    }
+
+    /// Set all bits to the desired value.
+    fn fill(&mut self, value: bool) {
+        self.board().fill(value);
+    }
+
+    fn or(&self, other: &impl BitBoard) -> Result<impl BitBoard, DimensionMismatch>;
+    fn and(&self, other: &impl BitBoard) -> Result<impl BitBoard, DimensionMismatch>;
+
+    /// Set the value at index [row, col] to be the `new_val`.
+    fn set(&mut self, row: usize, col: usize, value: bool) {
+        let new_ind = self.index_of(row, col);
+        self.board().set(new_ind, value);
+    }
+    /// Set an entire column to a certain value
+    fn set_col(&mut self, col: usize, value: bool) {
+        // For each row
+        for r_idx in 0..self.n_rows() {
+            // Calculate the index
+            let idx = (r_idx * self.n_cols()) + col;
+            self.board().set(idx, value);
+        }
+    }
+
+    /// Set an entire row to a certain value
+    fn set_row(&mut self, row: usize, value: bool) {
+        // For each column in the row
+        for cidx in 0..self.n_cols() {
+            // Calculate the index
+            let idx = (row * self.n_cols()) + cidx;
+            self.board().set(idx, value);
+        }
+    }
+
+    /// Will set the neighbors immediately above, below, left, and right to `value`. If
+    /// the neighbor is out of bounds, nothing will happen
+    fn set_cardinal_neighbors(&mut self, row: usize, col: usize, value: bool) {
+        // Above
+        if row > 0 {
+            self.set(row - 1, col, value);
+        }
+
+        // Below
+        if row < self.n_rows() - 1 {
+            self.set(row + 1, col, value);
+        }
+
+        // Left
+        if col > 0 {
+            self.set(row, col - 1, value);
+        }
+
+        // Right
+        if col < self.n_cols() - 1 {
+            self.set(row, col + 1, value);
+        }
+    }
+
+    /// Set just the spots diagonal from the given position to `value`. If
+    /// the neighbor is out of bounds, nothing will happen
+    fn set_diagonals(&mut self, row: usize, col: usize, value: bool) {
+        // Above left
+        if row > 0 && col > 0 {
+            self.set(row - 1, col - 1, value);
+        }
+
+        // Above right
+        if row > 0 && col < self.n_cols() - 1 {
+            self.set(row - 1, col + 1, value);
+        }
+
+        // Below left
+        if row < self.n_rows() - 1 && col > 0 {
+            self.set(row + 1, col - 1, value);
+        }
+
+        // Below right
+        if row < self.n_rows() - 1 && col < self.n_cols() - 1 {
+            self.set(row + 1, col + 1, value);
+        }
+    }
+
+    /// Set the cardinal neighbors and the diagonal neighbors to `value`. If
+    /// the neighbor is out of bounds, nothing will happen
+    fn set_all_neighbors(&mut self, row: usize, col: usize, value: bool) {
+        self.set_cardinal_neighbors(row, col, value);
+        self.set_diagonals(row, col, value);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@ use std::fmt;
 
 use bitvec::prelude::*;
 
+use crate::bitboard::BitBoard;
+
+pub mod bitboard;
+
 #[derive(Debug)]
 pub struct DimensionMismatch;
 
@@ -53,35 +57,20 @@ impl fmt::Display for BitBoardDyn {
     }
 }
 
-impl BitBoardDyn {
-    /// Create a new empty board with `n_rows` and `n_cols`.
-    pub fn new(n_rows: usize, n_cols: usize) -> Self {
-        BitBoardDyn {
-            board: bitvec![0; n_rows * n_cols],
-            n_rows,
-            n_cols,
-        }
+impl BitBoard for BitBoardDyn {
+    fn n_rows(&self) -> usize {
+        self.n_rows
     }
 
-    /// Get the index that we can use to directly access a certain spot on the board
-    pub fn index_of(&self, row: usize, col: usize) -> usize {
-        assert!(
-            row <= (self.n_rows - 1),
-            "row cannot be greater than n_rows"
-        );
-        assert!(
-            col <= (self.n_cols - 1),
-            "col cannot be greater than n_cols"
-        );
-        (row * self.n_cols) + col
+    fn n_cols(&self) -> usize {
+        self.n_cols
     }
 
-    /// Set all bits to the desired value.
-    pub fn fill(&mut self, value: bool) {
-        self.board.fill(value);
+    fn board(&mut self) -> &mut BitSlice {
+        &mut self.board
     }
 
-    pub fn or(&self, other: &BitBoardDyn) -> Result<BitBoardDyn, DimensionMismatch> {
+    fn or(&self, other: &BitBoardDyn) -> Result<BitBoardDyn, DimensionMismatch> {
         if (self.n_rows != other.n_rows) || (self.n_cols != other.n_cols) {
             return Err(DimensionMismatch);
         }
@@ -90,7 +79,7 @@ impl BitBoardDyn {
         Ok(new_board)
     }
 
-    pub fn and(&self, other: &BitBoardDyn) -> Result<BitBoardDyn, DimensionMismatch> {
+    fn and(&self, other: &BitBoardDyn) -> Result<BitBoardDyn, DimensionMismatch> {
         if (self.n_rows != other.n_rows) || (self.n_cols != other.n_cols) {
             return Err(DimensionMismatch);
         }
@@ -98,86 +87,16 @@ impl BitBoardDyn {
         new_board.board = self.board.clone() & other.board.clone();
         Ok(new_board)
     }
+}
 
-    /// Set the value at index [row, col] to be the `new_val`.
-    pub fn set(&mut self, row: usize, col: usize, value: bool) {
-        let new_ind = self.index_of(row, col);
-        self.board.set(new_ind, value);
-    }
-
-    /// Set an entire column to a certain value
-    pub fn set_col(&mut self, col: usize, value: bool) {
-        // For each row
-        for r_idx in 0..self.n_rows {
-            // Calculate the index
-            let idx = (r_idx * self.n_cols) + col;
-            self.board.set(idx, value);
+impl BitBoardDyn {
+    /// Create a new empty board with `n_rows` and `n_cols`.
+    pub fn new(n_rows: usize, n_cols: usize) -> Self {
+        BitBoardDyn {
+            board: bitvec![0; n_rows * n_cols],
+            n_rows,
+            n_cols,
         }
-    }
-
-    /// Set an entire row to a certain value
-    pub fn set_row(&mut self, row: usize, value: bool) {
-        // For each column in the row
-        for cidx in 0..self.n_cols {
-            // Calculate the index
-            let idx = (row * self.n_cols) + cidx;
-            self.board.set(idx, value);
-        }
-    }
-
-    /// Will set the neighbors immediately above, below, left, and right to `value`. If
-    /// the neighbor is out of bounds, nothing will happen
-    pub fn set_cardinal_neighbors(&mut self, row: usize, col: usize, value: bool) {
-        // Above
-        if row > 0 {
-            self.set(row - 1, col, value);
-        }
-
-        // Below
-        if row < self.n_rows - 1 {
-            self.set(row + 1, col, value);
-        }
-
-        // Left
-        if col > 0 {
-            self.set(row, col - 1, value);
-        }
-
-        // Right
-        if col < self.n_cols - 1 {
-            self.set(row, col + 1, value);
-        }
-    }
-
-    /// Set just the spots diagonal from the given position to `value`. If
-    /// the neighbor is out of bounds, nothing will happen
-    pub fn set_diagonals(&mut self, row: usize, col: usize, value: bool) {
-        // Above left
-        if row > 0 && col > 0 {
-            self.set(row - 1, col - 1, value);
-        }
-
-        // Above right
-        if row > 0 && col < self.n_cols - 1 {
-            self.set(row - 1, col + 1, value);
-        }
-
-        // Below left
-        if row < self.n_rows - 1 && col > 0 {
-            self.set(row + 1, col - 1, value);
-        }
-
-        // Below right
-        if row < self.n_rows - 1 && col < self.n_cols - 1 {
-            self.set(row + 1, col + 1, value);
-        }
-    }
-
-    /// Set the cardinal neighbors and the diagonal neighbors to `value`. If
-    /// the neighbor is out of bounds, nothing will happen
-    pub fn set_all_neighbors(&mut self, row: usize, col: usize, value: bool) {
-        self.set_cardinal_neighbors(row, col, value);
-        self.set_diagonals(row, col, value);
     }
 }
 
